b26ab3ff1e068c9ca4859612eeea1cd857f38ea6
Manish Meganathan <meganathan.manish@gmail.com>
2022-08-06 05:02:06 +0530

    Clean Collection Methods & Implement DocRef
    - Refactored newDocRef to be unexported
    - Refactored code structure of GetDocument, GetCollection and RemoveCollection methods of Collection and added code docs
    - Refactored Parent method of Collection
    - Added utility functions from adding and stripping file extension names
    - Implement Get, Set, Remove, Exists and Parent methods of DocRef

--- collection.go
+++ collection.go
@@ -27,7 +27,7 @@ func (collection *Collection) ListDocuments() ([]*DocRef, error) {
 		// If the file descriptor is a file, create a new DocRef
 		// and append into the accumulator
 		if !file.IsDirectory {
-			doc, err := NewDocRef(file, collection.client)
+			doc, err := newDocRef(file, collection.client)
 			if err != nil {
 				return nil, fmt.Errorf("failed to create docref for '%v': %w", file.Hash, err)
 			}
@@ -61,62 +61,63 @@ func (collection *Collection) ListCollections() ([]*Collection, error) {
 	return collections, nil
 }
 
+// GetDocument returns a DocRef to the document in the collection with the given name.
+// If the document does not exist, an error is thrown unless allowCreate is set, in which
+// case, a blank document is created and stored before a reference to it is returned.
 func (collection *Collection) GetDocument(name string, allowCreate bool) (*DocRef, error) {
-	// FileStatus on the pathJoin(collection.path, name)
-	// Verify file is not Directory
-	// if file.Exist() == false
-	// 	 allowCreate is true -> WriteFile at the above path
-	//   else -> throw error
-	// else wrap file descriptor into DocRef and return
-	path := pathJoin(collection.Path(), name)
-
-	fileDescriptor, err := collection.client.FileStatus(path)
+	// Create path to the document and get its file stat
+	path := pathJoin(collection.Path(), extAdd(name))
+	file, err := collection.client.FileStatus(path)
 	if err != nil {
-		return nil, fmt.Errorf("error fetching file descriptor: %w", err)
+		return nil, fmt.Errorf("failed to get document: filestat check failed %w", err)
 	}
 
-	if fileDescriptor.Exists() {
-		return NewDocRef(fileDescriptor, collection.client)
-	}
+	if !file.Exists() {
+		// If the file does not exist and allowCreate is not set, return error
+		if !allowCreate {
+			return nil, fmt.Errorf("failed to get document: does not exist")
+		}
 
-	if allowCreate {
-		fileDescriptors, err := collection.client.WriteFile([]byte{}, path, nil)
+		// allowCreate is set, so create a blank file at the path
+		files, err := collection.client.WriteFile([]byte{}, path, nil)
 		if err != nil {
 			return nil, fmt.Errorf("error creating new file: %w", err)
 		}
 
-		return NewDocRef(fileDescriptors[0], collection.client)
+		// Create a DocRef for the newly written file and return it
+		return newDocRef(files[0], collection.client)
 	}
 
-	return nil, fmt.Errorf("error document not found")
+	// Create a new DocRef for the existing file and return it
+	return newDocRef(file, collection.client)
 }
 
+// GetCollection attempts to retrieve a Collection of the given name from within the calling collection.
+// If the collection does not exist, it will be created.
 func (collection *Collection) GetCollection(name string) (*Collection, error) {
-	// Generate a new path from collection.path
-	// Create a collection for the new path
-	// Check if directory already exists by calling makeDirectory
-	// Return the new collection
+	// Create Collection object with path to specified collection
 	path := pathJoin(collection.Path(), name)
-	newCollection := &Collection{collection.client, pathSplit(path)}
+	subcol := &Collection{collection.client, pathSplit(path)}
 
 	// Attempt to make the directory at the specified path
 	if err := collection.client.MakeDirectory(path); err != nil {
 		// If directory already exist, expect a 400 error with a specific message,
 		// This indicated that the collection already exists and need not be created.
 		if err.Error() == "non-ok response [400]: directory exist | directory already exist" {
-			return newCollection, nil
+			return subcol, nil
 		}
 
 		return nil, fmt.Errorf("failed to create collection: %w", err)
 	}
 
-	return newCollection, nil
+	return subcol, nil
 }
 
+// RemoveCollection removes a Collection from within the calling Collection.
+// Calling it is idempotent and is a no-op if the collection does not exist.
 func (collection *Collection) RemoveCollection(name string) error {
-	// Remove with RemoveDirectory() enabled for pathJoin(collection.path, name)
+	// Create path to the sub-collection and attempt to remove it
 	path := pathJoin(collection.Path(), name)
-
 	if err := collection.client.RemoveFile(path, 0, moibit.RemoveDirectory()); err != nil {
 		return fmt.Errorf("error removing collection: %w", err)
 	}
@@ -124,18 +125,26 @@ func (collection *Collection) RemoveCollection(name string) error {
 	return nil
 }
 
+// Parent returns the parent Collection for the collection.
+// Returns an error if the collection's parent would be the root.
+// Note: Use the DocStore for accessing other collections in the root.
 func (collection *Collection) Parent() (*Collection, error) {
-	if pathJoin(collection.path...) == "/" {
-		return nil, fmt.Errorf("collection has not parent")
+	// If the path elements of the collection is 1, i.e, only
+	// contains the collection name with no parent, return an error
+	if len(collection.path) == 1 {
+		return nil, fmt.Errorf("collection parent is the docstore root")
 	}
 
+	// Create a collection with the last path element removed
 	return &Collection{collection.client, collection.path[:1]}, nil
 }
 
+// Path returns a path to the collection directory
 func (collection *Collection) Path() string {
 	return pathJoin(collection.path...)
 }
 
+// pathSplit is a utility function for splitting a path into its elements
 func pathSplit(path string) []string {
 	paths := make([]string, 0)
 	for _, split := range strings.Split(path, "/") {
@@ -147,6 +156,17 @@ func pathSplit(path string) []string {
 	return paths
 }
 
+// pathJoin is a utility function for joining path elements into a string
 func pathJoin(paths ...string) string {
 	return fmt.Sprintf("/%v", strings.Join(paths, "/"))
+}
+
+// extAdd is a utility function for adding the '.json' extension to a file name
+func extAdd(file string) string {
+	return strings.Join([]string{file, "json"}, ".")
+}
+
+// extRemove is a utility function for removing the '.json' extension to a file name
+func extRemove(filename string) string {
+	return strings.TrimPrefix(strings.Split(filename, ".json")[0], "/")
 }
--- docref.go
+++ docref.go
@@ -13,45 +13,71 @@ type DocRef struct {
 	file   moibit.FileDescriptor
 }
 
-func NewDocRef(file moibit.FileDescriptor, client *moibit.Client) (*DocRef, error) {
+// newDocRef generates and returns a new DocRef instance for a given MOIBit File Descriptor and Client
+func newDocRef(file moibit.FileDescriptor, client *moibit.Client) (*DocRef, error) {
 	// Fail if the file desc is for a directory
 	if file.IsDirectory {
 		return nil, fmt.Errorf("cannot create DocRef from directory")
 	}
 
+	// Create a DocRef with the path to the document including its name and directory
 	return &DocRef{pathSplit(pathJoin(file.Directory, file.Path)), client, file}, nil
 }
 
-func (docref *DocRef) Get() (*Document, error) {
-	// Read file at docref.Path()
-	// Pass Bytes into NewDocument
-	// Return the Document
+// Get attempts to retrieve the Document at the DocRef
+func (docref *DocRef) Get() (Document, error) {
+	// ReadFile at the path of DocRef with version specified by the FileDescriptor
+	data, err := docref.client.ReadFile(docref.Path(), docref.file.Version)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	// Create a new Document with the data from the file
+	doc, err := NewDocument(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
 }
 
-func (docref *DocRef) Set(doc *Document) error {
-	// Serialize doc.data
-	// Write file at docref.Path()
+// Set attempts to set the given Document as the data for DocRef
+func (docref *DocRef) Set(doc Document) error {
+	// Get the JSON Bytes for the Document
+	data, err := doc.GetJSON()
+	if err != nil {
+		return err
+	}
+
+	// WriteFile to path specified in DocRef
+	if _, err = docref.client.WriteFile(data, docref.Path()); err != nil {
+		return err
+	}
 
 	return nil
 }
 
+// Remove attempts to remove the Document at DocRef
 func (docref *DocRef) Remove() error {
-	// Remove file at docref.Path()
+	// RemoveFile at path specified by DocRef with version from the FileDescriptor
+	if err := docref.client.RemoveFile(docref.Path(), docref.file.Version); err != nil {
+		return err
+	}
 
 	return nil
 }
 
+// Exists returns whether a Document exists at DocRef
 func (docref *DocRef) Exists() bool {
-	return false
-	//return docref.file.Exists()
+	return docref.file.Exists()
 }
 
+// Parent returns the parent Collection of Document at DocRef
 func (docref *DocRef) Parent() *Collection {
 	return &Collection{docref.client, docref.path[:1]}
 }
 
+// Path returns the path to the Document at DocRef
 func (docref *DocRef) Path() string {
 	return pathJoin(docref.path...)
 }
